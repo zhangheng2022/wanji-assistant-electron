6b92ab41e9001b5ffbdc3a1c18cff3243742f2af
weiyang <962271461@qq.com>
2025-07-16 18:46:28 +0800

    commit init

--- src/main/index.ts
+++ src/main/index.ts
@@ -44,18 +44,17 @@ function createWindow(): void {
   //   mainWindow.webContents.openDevTools()
   // }
   // mainWindow.webContents.openDevTools()
-
-  log.initialize()
-  console.log = log.log
-  console.error = log.error
-  console.warn = log.warn
-  console.info = log.info
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(async () => {
+  log.initialize()
+  console.log = log.log
+  console.error = log.error
+  console.warn = log.warn
+  console.info = log.info
   // Set app user model id for windows
   electronApp.setAppUserModelId('com.electron')
   if (process.platform === 'darwin') {
