87271ba6b5ef1565534f5d2e29e6fabc7dd4387c
weiyang <962271461@qq.com>
2025-07-18 17:28:11 +0800

    commit

--- scripts/afterPack.mjs
+++ scripts/afterPack.mjs
@@ -36,12 +36,14 @@ export default async function afterPack(context) {
       } catch (err) {
         console.warn(`❌ 设置执行权限失败: ${fullPath}`, err.message)
       }
-      // try {
-      //   execSync(`codesign --force --sign - "${fullPath}"`)
-      //   console.log(`🔏 已签名: ${fullPath}`)
-      // } catch (err) {
-      //   console.warn(`❌ 签名失败: ${fullPath}`, err.message)
-      // }
+      try {
+        execSync(
+          `codesign --force --sign "Developer ID Application: Hangzhou TENGWEI Technology Co., Ltd (Z7B5LMFWHJ)" "${fullPath}"`
+        )
+        console.log(`🔏 已签名: ${fullPath}`)
+      } catch (err) {
+        console.warn(`❌ 签名失败: ${fullPath}`, err.message)
+      }
     }
   }
 }
